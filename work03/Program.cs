07f8f671e20cc5c2a548ca638e8df0c97de32f1b
Rimma <mudalieva@yandex.ru>
2023-01-29 15:11:12 +0400

    решение задачи №38

---
+++ work03/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] arr = GetRandomArray(5);
+
+string str = string.Join(", ", arr);
+System.Console.Write("[" + str + "] -> " + GetDiffMaxMin(arr));
+
+int[] GetRandomArray(int length) 
+{
+    int[] result = new int[length];
+
+    for (int i=0; i<length; i++) 
+    {
+        result[i] = new Random().Next(0, 100);
+    }
+    return result;
+}
+
+int GetDiffMaxMin(int[] array)
+{   
+    int min = array[0];  
+    int max = 0; 
+    int sum = 0;
+     
+    for(int i = 0; i < array.Length; i++)    
+    {       
+        if (array[i] < min)
+            min = array[i];
+
+        if (array[i] > max)
+            max = array[i];
+
+        sum = max - min;       
+    }    
+    return sum;   
+}
